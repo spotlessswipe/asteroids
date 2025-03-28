d2885b835ddf8384a4ada6c866766bee03583a86
spotlessswipe <spotlessswipe@pm.me>
2025-03-28 07:57:02 +0100

    Groups

--- main.py
+++ main.py
@@ -10,15 +10,26 @@ def main():
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(x , y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+
+        updatable.update(dt)
 
         screen.fill("black")
+
+        for element in drawable:
+            element.draw(screen)
+
         player.draw(screen)
         pygame.display.flip()
 
